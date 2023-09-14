a721c9dffe055078d958d3a31d2ea2f8ff6d9505
jagadishmunusamy <143987275+jagadishmunusamy@users.noreply.github.com>
2023-09-14 23:29:12 +0530

    Add files via upload

---
+++ Rock_paper_sissors_game.py
@@ -0,0 +1,57 @@
+import random
+
+while True:
+    
+    choices = ["rock","paper","scissor"]
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("rock or paper or scissor ? : ").lower()
+
+    if player == computer:
+        print("computer picks ---> " + computer )
+        print("player picks ---> " + player)
+        print("Match Tie!")
+    elif player == "rock":
+        if computer == "paper":
+             print("computer picks ---> " + computer )
+             print("player picks ---> " + player)
+             print("You Loose!")
+        if computer == "scissor":
+            print("computer picks ---> " + computer )
+            print("player picks ---> " + player)
+            print("You loose!")
+    elif player == "paper":
+        if computer == "rock":
+            print("computer picks ---> " + computer )
+            print("player picks ---> " + player)
+            print("You Win!")
+        if computer == "scissor":
+            print("computer picks ---> " + computer )
+            print("player picks ---> " + player)
+            print("You Loose!")
+    elif player == "scissor":
+        if computer == "rock":
+            print("computer picks ---> " + computer )
+            print("player picks ---> " + player)
+            print("You Loose!")
+        if computer == "paper":
+            print("computer picks ---> " + computer )
+            print("player picks ---> " + player)
+            print("You Win!")
+
+    play_again = input("play again? (yes/no) : ").lower()
+    if play_again != "yes":
+        break
+print("bye")
+        
+
+        
+        
+        
+    
+ 
+    
+
